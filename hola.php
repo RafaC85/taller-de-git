9bb0799b982013965ad615f10206e8cc250c879f
Rafa <rcasfer0805@g.educaand.es>
2025-10-23 13:51:31 +0200

    Programa interactivo

--- hola.php
+++ hola.php
@@ -2,6 +2,7 @@
 // Autor Rafa
 // Esto es un comentario
 // El nombre por defecto es Mundo
-$nombre = isset($argv[1])? $argv[1]: "Mundo";
+print "Introduce tu nombre";
+$nombre= trim(fgets(STDIN));
 @print "Hola {$nombre}\n";
 ?>
